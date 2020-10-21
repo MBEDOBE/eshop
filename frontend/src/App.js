def6ae79b32eb2c9d82b68293fde5fdc4c52de2f
MBEDOBE <danielbushbaby@gmail.com>
2020-10-21 12:37:07 +0000

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,139 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">eshop</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div>
+          <div className="row center">
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+            <div className="card">
+              <a href="product.html">
+                
+              <img className="medium" src="./images/p1.jpg" alt="product" />
+              </a>
+              <div className="card-body">
+                <a href="product.html">
+                  <h2>Nike Slim Shirts</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star-half-o"></i> </span>
+                </div>
+                <div className="price">₵120</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">© 2020 REIN IT - All rights reserved</footer>
     </div>
   );
 }
