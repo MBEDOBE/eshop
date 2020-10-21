2ed0d778185b786a3ff2a896a128be297d60db4d
MBEDOBE <danielbushbaby@gmail.com>
2020-10-21 18:31:21 +0000

    update

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -11,7 +11,7 @@ export default function ProductScreen(props) {
     }
     return (
         <div>
-            <Link to="/">Back to result</Link>
+            <Link to="/">Back to results</Link>
             <div className="row top">
                 <div className="col-2">
                     <img className="large" src={product.image} alt={product.name}></img>
