31ce29887b41d0683cefa298bff4d5f5cec0e34b
MBEDOBE <danielbushbaby@gmail.com>
2020-10-21 18:27:38 +0000

    changes to product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
 import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
@@ -18,14 +18,9 @@ function App() {
           </div>
         </header>
         <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
-          <div>
-            <div className="row center">
-              {data.products.map((product) => (
-                <Product key={product._id} product={product}></Product>
-              ))}
-            </div>
-          </div>
+
         </main>
         <footer className="row center">© 2020 REIN IT - All rights reserved</footer>
       </div>
