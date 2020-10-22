5f51e559a0fce39a21d9921079a85e3333be1ae1
MBEDOBE <danielbushbaby@gmail.com>
2020-10-22 08:17:01 +0000

    changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 
-//const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-    console.log(`Serve at http://localhost:5000`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
 });
