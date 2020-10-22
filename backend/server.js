e5a124a9112bb323bfddf07e3841e1b51051a0b5
MBEDOBE <danielbushbaby@gmail.com>
2020-10-22 08:07:06 +0000

    new changes

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+//const port = process.env.PORT || 5000;
+app.listen(5000, () => {
+    console.log(`Serve at http://localhost:5000`);
+});
