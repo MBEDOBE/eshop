7cefb1d662c4258664b71439733b808f24a20ba7
MBEDOBE <danielbushbaby@gmail.com>
2020-11-02 04:40:26 +0000

    updated  actions, reducers and constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import data from './data';
+import { productListReducer } from './reducers/productReducers';
 
 const initialState = {};
-const reducer = (state, action) => {
-    return { products: data.products };
-};
+const reducer = combineReducers({
+    productList: productListReducer,
+});
+
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
