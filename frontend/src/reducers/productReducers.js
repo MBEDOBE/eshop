df72e9dc20ec822a6fcc8635e5bfef0c1d65a25a
MBEDOBE <danielbushbaby@gmail.com>
2020-11-06 09:11:45 +0000

    product details update

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-const { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS }= require("../constants/productConstants");
+const { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_FAIL, PRODUCT_DETAILS_SUCCESS } = require("../constants/productConstants");
 
 export const productListReducer = (
     state = { loading: true, products: [] },
@@ -15,3 +15,19 @@ export const productListReducer = (
             return state;
     }
 };
+
+export const productDetailsReducer = (
+    state = { product: {}, loading: true },
+    action
+) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return { loading: true };
+        case PRODUCT_DETAILS_SUCCESS:
+            return { loading: false, product: action.payload };
+        case PRODUCT_DETAILS_FAIL:
+            return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,18 +1,19 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-    reducer, 
-    initialState, 
+    reducer,
+    initialState,
     composeEnhancer(applyMiddleware(thunk)));
 
 export default store;
