d25186f8a253d41474787351c239282b992c40f0
MBEDOBE <danielbushbaby@gmail.com>
2020-11-27 19:38:28 +0000

    cart updates

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,7 +1,7 @@
-import Axios from "axios"; 
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import Axios from "axios";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
-export const addToCart = (productId, qty) => async (dispatch,getState) => {
+export const addToCart = (productId, qty) => async (dispatch, getState) => {
     const { data } = await Axios.get(`/api/products/${productId}`);
     dispatch({
         type: CART_ADD_ITEM,
@@ -15,5 +15,9 @@ export const addToCart = (productId, qty) => async (dispatch,getState) => {
         },
     });
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-}
+};
+export const removeFromCart = (productId) => (dispatch, getState) => {
+    dispatch({ type: CART_REMOVE_ITEM, payload: productId });
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
 
