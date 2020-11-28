d5861c2de2d3a110b923de4d2fefe4cd037774c3
MBEDOBE <danielbushbaby@gmail.com>
2020-11-28 19:44:10 +0000

    updated userRouter

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 
 userRouter.get('/seed',
     expressAsyncHandler(async (req, res) => {
-        await User.remove({});
+        //await User.remove({});
         const createdUsers = await User.insertMany(data.users);
         res.send({ createdUsers });
     })
