586e94d2129e1c9b8b4410f85bb9f597eebdb449
MBEDOBE <danielbushbaby@gmail.com>
2021-05-18 02:34:29 +0000

    shipping screen and payment methods

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from '../constants/cartConstants';
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
     switch (action.type) {
@@ -20,9 +20,10 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
                 ...state,
                 cartItems: state.cartItems.filter((x) => x.product !== action.payload),
             };
-
         case CART_SAVE_SHIPPING_ADDRESS:
             return { ...state, shippingAddress: action.payload };
+        case CART_SAVE_PAYMENT_METHOD:
+            return { ...state, paymentMethod: action.payload };
         default:
             return state;
     }
