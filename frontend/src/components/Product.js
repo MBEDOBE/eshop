871001a81e7d37eb7a1db865e8e8eca7d66ecd7d
MBEDOBE <danielbushbaby@gmail.com>
2022-05-02 18:29:24 +0000

    ui changes

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
+import Button from 'react-bootstrap/Button';
 
 export default function Product(props) {
-    const { product } = props;
-    return (
-        <div key={product._id} className="card">
-            <Link to={`/product/${product._id}`}>
-                <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                />
-            </Link>
-            <div className="card-body">
-                <Link to={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                </Link>
-                <Rating
-                    rating={product.rating}
-                    numReviews={product.numReviews}
-                ></Rating>
-                <div className="price">GH₵{product.price}</div>
-            </div>
-        </div>
-    )
+  const { product } = props;
+  return (
+    <div key={product._id} className="card">
+        <img className="medium" src={product.image} alt={product.name} />
+      <div className="card-body">
+          <h2>{product.name}</h2>
+        <Rating
+          rating={product.rating}
+          numReviews={product.numReviews}
+        ></Rating>
+        <div className="price">GH₵{product.price}</div>
+        <Link to={`/product/${product._id}`}>
+          <div className="card-btn">
+            <button>Add to Cart</button>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
