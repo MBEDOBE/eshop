2737e91427a1d10699781d8a06223b8548d0335e
D. Mbedobe <danielbushbaby@gmail.com>
2022-07-19 11:37:03 +0000

    top-seller-carousel

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -23,6 +23,7 @@ import {
   userListReducer,
   userRegisterReducer,
   userSigninReducer,
+  userTopSellerListReducer,
   userUpdateProfileReducer,
   userUpdateReducer,
 } from './reducers/userReducers';
@@ -65,6 +66,7 @@ const reducer = combineReducers({
   orderDeliver: orderDeliverReducer,
   userList: userListReducer,
   userDelete: userDeleteReducer,
+  userTopSellersList: userTopSellerListReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
